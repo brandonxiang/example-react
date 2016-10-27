76e152cc530ecc46ec782db5a31c33dc8daa1c9d
brandonxiang <1542453460@qq.com>
2016-10-27 15:19:22 +0800

    init

---
+++ src/Wrap.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import App from './App.js'
+
+class Warp extends Component{
+  render(){
+    return (
+      <div>
+      <App title="Hello World"/>
+      </div>
+    )
+  }
+}
+
+export default Warp;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Wrap from './Wrap';
+import './index.css';
+
+ReactDOM.render(
+  <Wrap />,
+  document.getElementById('root')
+);
