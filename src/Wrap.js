834566608e0d104ff0c7cc4a55344aac40d0d681
brandonxiang <1542453460@qq.com>
2016-11-14 11:24:58 +0800

    add a example for react-color

--- src/Wrap.js
+++ src/Wrap.js
@@ -1,14 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import App from './App.js'
+import { ChromePicker } from 'react-color'
+import ButtonExample from './ButtonExample'
 
-class Warp extends Component{
-  render(){
-    return (
-      <div>
-      <App title="Hello World"/>
-      </div>
-    )
-  }
+class Warp extends Component {
+    render() {
+        return ( < div >
+            < App title = "Hello World" / >
+            < ChromePicker / >
+            < ButtonExample / >
+            < /div>
+        )
+    }
 }
 
 export default Warp;
