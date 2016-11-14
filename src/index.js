a65209cf5e5abcbf14a0accabf99bb219cbba63e
brandonxiang <1542453460@qq.com>
2016-11-15 00:12:22 +0800

    fix app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import List from './List';
-import './index.css';
+import { render } from 'react-dom';
 
-ReactDOM.render( < List / > ,
-    document.getElementById('root')
-);
+import './index.css';
+import { Router, Route, hashHistory } from 'react-router'
+import List from './modules/List'
+import App from './modules/App'
+import ButtonExample from './modules/ButtonExample.js'
+
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={List}>
+      <Route path="/app" component={App}/>
+      <Route path="/buttonexample" component={ButtonExample}/>
+    </Route>
+  </Router>
+), document.getElementById('app'))
