0c5c29eaaf9916ca5e76bd75ca61cd30dcbdf3a9
brandonxiang <1542453460@qq.com>
2016-11-15 10:24:46 +0800

    fix bug

--- src/modules/List.js
+++ src/modules/List.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>React Router Tutorial</h1>
-        <ul role="nav">
-          <li><Link to="/app">App</Link></li>
-          <li><Link to="/buttonexample">ButtonExample</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
+    render() {
+        return (
+          < div >
+            < h1 > React Router Tutorial < /h1>
+            < ul role = "nav" >
+             < li >
+             < Link to = "/app" > App < /Link>
+             </li >
+             < li >
+             < Link to = "/buttonexample" > ButtonExample < /Link>
+             </li >
+             < /ul>
+             < /div >
+        )
+    }
 })
