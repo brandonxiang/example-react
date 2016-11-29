55ce3bd98c17506b40d6973c51d9125bd1e6b9f6
brandonxiang <1542453460@qq.com>
2016-11-29 16:58:34 +0800

    style for SliderNum

--- src/modules/SliderNum.js
+++ src/modules/SliderNum.js
@@ -24,12 +24,13 @@ class SliderNum extends React.Component{
   render() {
     return (
       <Row>
-       <Col span={4}>
-          <InputNumber min={this.props.min} max={this.props.max} step={this.props.step} 
+       <Col span={18}>
+          <InputNumber style={{width:'100%'}} min={this.props.min} max={this.props.max} step={this.props.step}
             value={this.state.inputValue} onChange={this.onChange.bind(this)}
           />
         </Col>
-        <Col span={2}>
+
+        <Col span={4} offset={2}>
           <Slider min={this.props.min} max={this.props.max} onChange={this.onChange.bind(this)} value={this.state.inputValue} />
         </Col>
       </Row>
